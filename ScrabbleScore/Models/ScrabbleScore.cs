02da3e3b830ae366fda51e0569abe9b72c96eac0
jakechaley <jakechaley@gmail.com>
2022-02-24 11:35:14 -0800

    pass test for LetterScore
    co-authored-by: Riley Shimp <rilryshimp@gmail.com>
    Co-authored-by: Drew Henderson <drewhendersonemail@gmail.com>

--- ScrabbleScore/Models/ScrabbleScore.cs
+++ ScrabbleScore/Models/ScrabbleScore.cs
@@ -12,8 +12,38 @@ namespace ScrabbleScore.Models
 
     public int LetterScore(string letter)
     {
-      return 0;
+      if (letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u" || letter == "l" || letter == "n" || letter == "r" || letter == "s" || letter == "t")
+      {
+        return 1;
+      }
+      else if (letter == "d" || letter == "g")
+      {
+        return 2;
+      }
+      else if (letter == "b" || letter == "c" || letter == "m" || letter == "p")
+      {
+        return 3;
+      }
+      else if (letter == "f" || letter == "h" || letter == "v" || letter == "w" || letter == "y")
+      {
+        return 4;
+      }
+      else if (letter == "k")
+      {
+        return 5;
+      }
+      else if (letter == "j" || letter == "x")
+      {
+        return 8;
+      }
+      else if (letter == "q" || letter == "z")
+      {
+        return 10;
+      }
+      else
+      {
+        return 0;
+      }
     }
   }
-  
 }
